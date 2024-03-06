c053f959355e4cccb5f90e8d443946d408e2e2d9
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-03-06 14:46:57 -0500

    Create TimeConversion.java

---
+++ TimeConversion.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class  TimeConversion{
+    public static void main (String[] args){
+        int time;
+        Scanner input = new Scanner(System.in);
+
+    }
+}
