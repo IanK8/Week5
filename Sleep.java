a677a48c10b0e9793d299b5673942ad5f3a3249f
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-03-07 14:34:28 -0500

    Update Sleep.java

---
+++ Sleep.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.text.NumberFormat;
+public class Sleep {
+    public static void main(String[] args){
+        int bYear;
+        int bMonth;
+        int bDay;
+        int cYear;
+        int cMonth;
+        int cDay;
+
+        Scanner input = new Scanner(System.in);
+        NumberFormat comma = NumberFormat.getIntegerInstance();
+
+        System.out.println("Enter your birthdate:");
+        System.out.print("Year: ");
+        bYear = input.nextInt();
+        System.out.print("Month: ");
+        bMonth = input.nextInt();
+        System.out.print("Day: ");
+        bDay = input.nextInt();
+        System.out.println("Enter today's date:");
+        System.out.print("Year: ");
+        cYear = input.nextInt();
+        System.out.print("Month: ");
+        cMonth = input.nextInt();
+        System.out.print("Day: ");
+        cDay = input.nextInt();
+
+        int timeAlive = ((cYear - bYear) * 365) + ((cMonth - bMonth) * 30) + (cDay - bDay);
+        int timeSlept = timeAlive * 8;
+
+        System.out.println("You have been alive for " + comma.format(timeAlive) + " days.");
+        System.out.println("You have slept for " + comma.format(timeSlept) + " hours.");
+
+
+    }
+}
