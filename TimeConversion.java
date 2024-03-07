813a5f59edfec43255087b8e73356676e378df2f
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-03-07 14:19:52 -0500

    Update TimeConversion.java

--- TimeConversion.java
+++ TimeConversion.java
@@ -4,6 +4,8 @@ public class  TimeConversion{
     public static void main (String[] args){
         int time;
         Scanner input = new Scanner(System.in);
-
+        System.out.print("Enter the time in minutes: ");
+        time = input.nextInt();
+        time = time % 
     }
 }
