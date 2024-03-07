3fc1fa2046905d0a1b80870b8e3ec554b9e8f3b9
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-03-07 14:34:35 -0500

    Update TimeConversion.java

--- TimeConversion.java
+++ TimeConversion.java
@@ -1,26 +1,16 @@
 import java.util.Scanner;
 
-<<<<<<< HEAD
-public class  TimeConversion{
-    public static void main (String[] args){
-        int time;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the time in minutes: ");
-        time = input.nextInt();
-        time = time % 
-=======
+
 public class TimeConversion {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the time in minutes: ");
-        int totalMinutes = scanner.nextInt();
-
-        int hours = totalMinutes / 60;
-        int minutes = totalMinutes % 60;
-
-        System.out.println("The time is: " + hours + ":" + (minutes < 10 / "0" + minutes : minutes));
->>>>>>> 96b835f372faf82c3a547168c04509591e9fd746
+    public static void main (String[] args){
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("Enter time in minutes: ");
+        int timeInMin = userInput.nextInt();
+        int onesMin = (timeInMin % 60) % 10;
+        int tensMin = (timeInMin % 60) / 10;
+        int timeInHours = (timeInMin - onesMin) / 60;
+        userInput.close();
+        System.out.println("The time is " + timeInHours + ":" + tensMin + onesMin);
     }
 }
-
